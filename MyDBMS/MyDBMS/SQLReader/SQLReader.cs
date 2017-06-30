eaff8aa922e1f5ab99db80d525e546801e236420
fangxianghl <1299883924@qq.com>
2017-06-30 14:08:37 +0800

    0.1.1

---
+++ MyDBMS/MyDBMS/SQLReader/SQLReader.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDBMS.SQLReader
+{
+    class SQLReader
+    {
+        public static DataTable readsql(string sql)
+        {
+            string[] all = new string[10];
+            all[0] = "select";
+            all[1] = "insert";
+            all[2] = "delete";
+            all[3] = "uqdate";
+            all[4] = "drop";
+            all[5] = "create";
+            char[] first = new char[10];
+            int i,j, k;
+            j = 0;
+            for (i = 0; sql[i] != '\0'; i++)
+            {
+                if (sql[i] != ' ')
+                {
+                    first[j] = sql[i];
+                }
+                if (j > 0 && sql[i] == ' ')
+                    break;
+            }
+            k = i;
+        }      
+
+    }
+}
